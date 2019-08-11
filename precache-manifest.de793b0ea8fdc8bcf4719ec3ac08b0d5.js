fb9302d4c4a22c8a5272f7317c1153716e2a2c36
SURYA <suryasaikandikonda@gmail.com>
2019-08-11 18:52:53 +0530

    Updates

---
+++ precache-manifest.de793b0ea8fdc8bcf4719ec3ac08b0d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2039343b448c09b484e01382c231d7",
+    "url": "/webiste-personal/index.html"
+  },
+  {
+    "revision": "61a77b430b1a1376af60",
+    "url": "/webiste-personal/static/css/main.2bdf2dbe.chunk.css"
+  },
+  {
+    "revision": "2616b4d26b92aa32c55e",
+    "url": "/webiste-personal/static/js/2.974e10d9.chunk.js"
+  },
+  {
+    "revision": "61a77b430b1a1376af60",
+    "url": "/webiste-personal/static/js/main.b558a95e.chunk.js"
+  },
+  {
+    "revision": "05cf359839a1ae258b99",
+    "url": "/webiste-personal/static/js/runtime~main.62428b39.js"
+  },
+  {
+    "revision": "fac7e06dc4ac7421af2d70a518922404",
+    "url": "/webiste-personal/static/media/CRR.fac7e06d.jpeg"
+  },
+  {
+    "revision": "dc3525f2cd27c918173e9a40ef437757",
+    "url": "/webiste-personal/static/media/Seminar.dc3525f2.jpg"
+  },
+  {
+    "revision": "9aa1702c3d85869ae6029f54946b4152",
+    "url": "/webiste-personal/static/media/SuryaImage.9aa1702c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webiste-personal/static/media/logo.5d5d9eef.svg"
+  }
+]);
